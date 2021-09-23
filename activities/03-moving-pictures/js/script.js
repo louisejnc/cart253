17ae45787d8c940b3141b61114b5eaea50db7864
louisejnc <louise.jeancampana@gmail.com>
2021-09-23 12:01:12 -0400

    A03 Activity completed

--- activities/03-moving-pictures/js/script.js
+++ activities/03-moving-pictures/js/script.js
@@ -1,9 +1,11 @@
 /**
-Title of Project
-Author Name
+Activity 03: Moving objects
+Louise JEAN-CAMPANA
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Two circles, the left one bigger and more transparent than the right,
+come in from either side of the screen, growing as they do so.
+They stop in the centre while still growing.
+The background goes from black to red.
 */
 
 "use strict";
@@ -15,19 +17,61 @@ Description of preload
 function preload() {
 
 }
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0
+};
 
+let circle1 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  speed: 1,
+  growthRate: 1,
+  fill: 255,
+  alpha: 200,
+};
 
+let circle2 = {
+  x: 500,
+  y: 250,
+  size: 75,
+  speed: -1,
+  sizeRatio: 0.5,
+  fill: 255,
+  alpha: 200
+};
 /**
 Description of setup
 */
 function setup() {
-
-}
-
+  createCanvas(500,500);
+  noStroke();
+};
 
 /**
 Description of draw()
 */
 function draw() {
+  //background
+  background(bg.r, bg.g, bg.b);
+  bg.r = map(circle1.size,100,width,0,255);
+
+  //left circle1
+  circle1.x += circle1.speed;
+  circle1.x = constrain(circle1.x, 0, width/2);
+  circle1.size += circle1.growthRate;
+  circle1.size = constrain(circle1.size, 0, width);
+  fill(circle1.fill, circle1.alpha);
+  ellipse(circle1.x, circle1.y, circle1.size);
+
+  //right circle2
+  circle2.x += circle2.speed;
+  circle2.x = constrain(circle2.x, width/2, width);
+  circle2.size = circle1.size * circle2.sizeRatio;
+  fill(circle2.fill, circle2.alpha);
+  ellipse(circle2.x, circle2.y, circle2.size);
+
 
 }
