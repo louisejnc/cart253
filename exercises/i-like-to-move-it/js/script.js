5abd4a53d36ad7bb37c6c6960b63e0de3ba851f4
louisejnc <louise.jeancampana@gmail.com>
2021-09-23 14:11:21 -0400

    Step 3 triangle created

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -21,17 +21,29 @@ let bg = {
 let square = {
   x: 180,
   y: -100,
-  fill: 0,
+  fill: 255,
   size: 200,
-  speed: 2
+  speed: 1
 }
 
+let t = {
+  x1: 330,
+  y1: 0,
+  x2: 380,
+  y2: 130,
+  x3: 200,
+  y3: 130,
+  fill: 255,
+  speed: 1.3
+}
+
+
 let circle = {
-  x: 250,
+  x: 265,
   y: -50,
-  fill: 0,
+  fill: 255,
   size: 100,
-  speed: 1
+  speed: 2
 }
 
 
@@ -40,7 +52,7 @@ let circle = {
 // Description of setup() goes here.
 function setup() {
   createCanvas(500,500);
-  noStroke();
+  //noStroke();
 }
 
 // draw()
@@ -52,15 +64,26 @@ function draw() {
   //square
   rectMode(CENTER);
   square.y += square.speed;
-  square.y = constrain(square.y,-100,400);
+  square.y = constrain(square.y,-500,400);
   fill(square.fill);
   rect(square.x, square.y, square.size);
+
+  //triangle
+  t.y1 += t.speed;
+  t.y2 += t.speed;
+  t.y3 += t.speed;
+  t.y1 = constrain(t.y1,-500,370);
+  t.y2 = constrain(t.y2,-500,500);
+  t.y3 = constrain(t.y3,-500,500);
+
+  fill(t.fill);
+  triangle(t.x1,t.y1,t.x2,t.y2,t.x3,t.y3);
 
 
   //circle
   ellipseMode(CENTER);
   circle.y += circle.speed;
-  circle.y = constrain(circle.y, -50, 450);
+  circle.y = constrain(circle.y, -500, 450);
   fill(circle.fill);
   ellipse(circle.x, circle.y, circle.size);
 
