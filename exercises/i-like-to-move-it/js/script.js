631809aed6b088d7ff012594906220eefc4bdfbf
louisejnc <louise.jeancampana@gmail.com>
2021-09-23 13:10:22 -0400

    1 step Circle created

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -1,22 +1,53 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+I like to move it!
+Louise JEAN-CAMPANA
 
-Here is a description of this template p5 project.
+Draw a square, a circle and a triangle.
+Differents sizes and colors
+They move from top to bottom
+constrain to stop their fall at the bottom of the canvas
+mouseX to change their sizes
+mouseY to change their colors
 **************************************************/
+
+let bg = {
+  r: 255,
+  g: 0,
+  b: 255
+}
+
+let circle = {
+  x: 250,
+  y: -50,
+  fill: 0,
+  size: 100,
+  speed: 1
+}
+
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(500,500);
+  noStroke();
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(bg.r, bg.g, bg.b);
+
+  //circle
+  ellipseMode(CENTER);
+  circle.y += circle.speed;
+  circle.y = constrain(circle.y, -50, 450);
+  fill(circle.fill);
+  ellipse(circle.x, circle.y, circle.size);
+
+
 
 }
