47705dfc2b31a75894fc7ccbf875dc7ba6ed2bdf
louisejnc <louise.jeancampana@gmail.com>
2021-09-23 01:07:20 -0400

    try to draw an alien

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -10,18 +10,56 @@ author, and this description to match your project!
 
 
 /**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
 Description of setup
 */
 function setup() {
+  createCanvas(600,600);
 
+  background(255,255,204);
+//  noStroke();
+  rectMode(CENTER);
+
+  fill(124,124,255);
+//  ellipse(300,500,330,200); ellipse(300,525,300,170);
+  rect(300,660,325,400,100,100);
+
+  ellipseMode(CENTER)
+  fill(160,160,255);
+  ellipse(300,200,400,300);
+  ellipse(300,210,400,290);
+  ellipse(300,220,400,280);
+  ellipse(300,230,395,270);
+  ellipse(300,240,390,260);
+  ellipse(300,250,380,250);
+  ellipse(300,260,370,240);
+  ellipse(300,270,360,230);
+  ellipse(300,280,350,220);
+  ellipse(300,290,340,210);
+  ellipse(300,300,330,200);
+  ellipse(300,310,320,190);
+  ellipse(300,320,310,180);
+  ellipse(300,330,300,170);
+  ellipse(300,340,290,160);
+  ellipse(300,350,280,150);
+  ellipse(300,360,270,140);
+  ellipse(300,370,260,130);
+  ellipse(300,385,190,120);
+  ellipse(300,400,170,110);
+  ellipse(300,415,150,100);
+  ellipse(300,427,135,90);
+
+
+
+
+
+  ellipse(300,440,120,80);
+  ellipse(300,450,110,70);
+
+  ellipse(300,460,100,60);
+  ellipse(300,470,90,50);
+
+
+  //ellipse(300,300,300,400);  ellipse(300,250,330,300);  ellipse(300,300,310,300)
 }
 
 
